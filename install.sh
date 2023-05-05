5b69d56ae439dd950acf2be483d9cd71c9bcf15c
Diego <diego.bcn.1993@gmail.com>
2023-05-05 23:47:18 +0200

    Add system to save bash_history with instalation script

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+EXECUTION_PATH='/usr/local/bin'
+cd ~/
+HOME_PATH=$(pwd)
+cd -
+
+if [ ! $(grep '\-save_history_installed$' ~/.bashrc | wc -l) -gt 0 ]; then 
+
+    ## Prepare service file
+    sed -i.bak 's|EXECUTION_PATH|'"$EXECUTION_PATH"'|g' save_history.service
+    sed -i.bak 's|HOME_PATH|'"$HOME_PATH"'|g' save_history.sh
+
+    ## Save execution script in selected path
+    sudo cp save_history.sh $EXECUTION_PATH
+
+    ## Save service file in /etc/systemd/system/
+    sudo cp save_history.service /etc/systemd/system/
+    
+    ## Insert artificial line into .bash_history every time a terminal is initiated
+    echo '' >> ~/.bashrc
+    echo '# Execute command to see in ~/.bash_history how many terminals are opened during the day -save_history_installed' >> ~/.bashrc
+    echo 'echo "#NEW TERMINAL" $(tty) $(date) $(hostname) $(pwd) >> ~/.bash_history # -save_history_installed' >> ~/.bashrc
+    echo '' >> ~/.bashrc
+
+    ## Append all the commands to ~/.bash_history real-time
+    echo '' >> ~/.bashrc
+    echo '# Append all the commands from different terminals to ~/.bash_history real-time -save_history_installed' >> ~/.bashrc
+    echo "export PROMPT_COMMAND='history -a' # -save_history_installed" >> ~/.bashrc
+    echo '' >> ~/.bashrc
+
+    ## Remove current parameters HISTSIZE and HISTFILESIZE
+    sed -i '/^HISTSIZE=/d' ~/.bashrc
+    sed -i '/^HISTFILESIZE=/d' ~/.bashrc
+
+    ## Set new values for history lenght and size
+    echo '' >> ~/.bashrc
+    echo "# Set history length with HISTSIZE and HISTFILESIZE -save_history_installed" >> ~/.bashrc
+    echo "HISTSIZE=100000" >> ~/.bashrc
+    echo "HISTFILESIZE=200000" >> ~/.bashrc
+    echo '' >> ~/.bashrc
+
+    ## Activate service
+    sudo systemctl enable save_history.service
+    sudo systemctl start save_history.service
+    #sudo systemctl status save_history.service
+    #sudo systemctl stop save_history.service
+    #sudo systemctl daemon-reload
+    #sudo journalctl -u save_history.service
+fi
---
+++ save_history.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#!/bin/bash
+
+
+echo Start saving history!
+
+## Define directory to save history
+home=HOME_PATH
+CLIhist=$home/CLIhistory
+
+## If GoodPractices directory does not exist, create it
+if ! [ -d $CLIhist ]; then
+    mkdir $CLIhist
+fi
+
+## Define function that saves the intended files
+function savehist {
+    ## Save date and hostname variables
+    d=$(date "+%Y-%m-%d")
+
+    ## Save history in appropriate directory
+    cat $home/.bash_history >> $CLIhist/$d.txt
+    cat /dev/null > $home/.bash_history
+    chmod 600 $CLIhist/$d.txt
+}
+
+## Count number of lines in .bash_history
+x=$(wc -l $home/.bash_history | awk '{ print $1 }')
+
+## If a minnimum work is detected (10 lines), execute previous function
+if [[ $x -gt 10 ]]; then savehist; fi
+
+echo Done!
+
